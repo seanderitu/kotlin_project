47d029904e2aa60008a3a686c7558d73237a011c
seanderitu <seangithaiga@gmail.com>
2023-03-14 12:24:54 +0300

    calculated area

---
+++ src/main/kotlin/area.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var length = 30
+    var width = 10
+    print(width*length)
+}
+
---
+++ src/main/kotlin/bmi.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+     var weight = 67
+     var height =1.7
+     print(weight/height)
+}
