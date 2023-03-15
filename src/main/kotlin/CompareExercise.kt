2a1b4170170a4b8112948e4b72dc97dec29bbce8
seanderitu <seangithaiga@gmail.com>
2023-03-15 12:04:44 +0300

    made comparisons

---
+++ src/main/kotlin/CompareExercise.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    print("Enter your Age")
+
+    var age:Int= readLine()!!.toInt()
+
+    if (age>=18){
+        print("Welcome to the club")
+    }
+    else{
+        print("Sorry we cannot admit underage")
+    }
+
+
+
+
+}
